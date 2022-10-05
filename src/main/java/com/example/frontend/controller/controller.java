e839af5de0a1ea83f00b025699190d29efe7ccb6
JimmyKristensen <siswanto@live.dk>
2022-10-05 10:00:02 +0200

    added ("/") to controller

--- src/main/java/com/example/frontend/controller/controller.java
+++ src/main/java/com/example/frontend/controller/controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 
-    @GetMapping
+    @GetMapping("/")
     String index(){
         return "index";
     }
