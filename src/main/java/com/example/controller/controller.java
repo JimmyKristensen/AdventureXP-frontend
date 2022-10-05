0e688cb70ac3e2401ac77afdc6aaae13eb1ffe8c
JimmyKristensen <siswanto@live.dk>
2022-10-05 11:33:14 +0200

    added thymeleaf dependency

--- src/main/java/com/example/controller/controller.java
+++ src/main/java/com/example/controller/controller.java
@@ -1,4 +1,4 @@
-package com.example.frontend.controller;
+package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +11,5 @@ public class controller {
     public String index(){
         return "index";
     }
-    @GetMapping("/hej")
-    @ResponseBody
-    public String index2(){
-        return "indwlerkgne";
-    }
-
 
 }
