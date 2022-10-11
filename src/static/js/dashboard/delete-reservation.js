60c42d2bed4209b85de07c5add86b09650e8d22c
vulpyalt <vulpyalt@gmail.com>
2022-10-11 10:06:09 +0200

    reservation delete js wip

---
+++ src/static/js/dashboard/delete-reservation.js
@@ -0,0 +1,35 @@
+'use strict'
+
+class ReservationRendererPost {
+    //endpoint url
+    endpointURL = "http://localhost:8080/api/v1/reservations/";
+    //constructor
+    constructor(data) {
+        this.data = data;
+    }
+    //async fetch, await response then call update
+    async postData(id) {
+        try {
+            let response = await fetch(this.endpointURL+id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({}),
+                redirect: 'follow'
+            });
+            this.data = await response.json();
+            console.table(this.data);
+        } catch (error) {
+            // Could not connect, try using the last data, we saved last time we were connected to remote endpoint.
+            console.log(`Failed posting data to remote endpoint ${this.endpointUrl}.`);
+        }
+    }
+}
+
+var reservationRendererPost = new ReservationRendererPost();
+
+function deleteFunction(id) {
+    reservationRendererPost.postData(id);
+    return confirm('Er du sikker på du vil slette?');
+}
