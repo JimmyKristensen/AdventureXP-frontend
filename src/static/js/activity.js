81ad0f6d07608b44c5b0af126de20aff1e21ad09
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2022-10-12 11:50:59 +0200

    Ændret fa ikoner til bootstrap ikoner og tilføjet login template i login.html

--- src/static/js/activity.js
+++ src/static/js/activity.js
@@ -1,33 +1,38 @@
-class ActivityRenderer{
+class ActivityRenderer {
 
     //endpoint url
     //endpointURL = "http://localhost:8080/api/v1/activities/";
     endpointURL = "https://adventurexp-backend.azurewebsites.net/api/v1/activities";
-    
+
     //constructor
-    constructor(data){
+    constructor(data) {
         this.data = data;
         this.fetchData();
     }
     //async fetch, await response then call update
-    async fetchData(){
+    async fetchData() {
         let response = await fetch(this.endpointURL);
         this.data = await response.json();
         this.updateUI();
         console.log(this.data)
     }
     //update ui elements
-    updateUI(){
+    updateUI() {
         //append data from function
-        
+
 
 
         let target = $('#actExpand')
 
         let card = `<div  class="table-responsive">
         <table class="table text-start">
+
             <tr>
-                <th>Description</th>
+                <th>Aktivitet type</th>
+                <td>${this.data[0].typeOfActivity}</td>
+            </tr>
+            <tr>
+                <th>Beskrivelse</th>
                 <td>${this.data[0].descriptionOfActivity}</td>
             </tr>
             <tr>
@@ -53,7 +58,7 @@ class ActivityRenderer{
         </table>
     </div>`;
 
-    target.append(card);
+        target.append(card);
     }
 
     setActivityData(index) {
